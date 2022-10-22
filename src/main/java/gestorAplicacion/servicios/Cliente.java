cd1cb0cffa9ed5635f7e3baef44550e03dad73c1
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2022-10-21 19:43:39 -0500

    agregado metodo corregir cliente

--- src/main/java/gestorAplicacion/servicios/Cliente.java
+++ src/main/java/gestorAplicacion/servicios/Cliente.java
@@ -13,6 +13,11 @@ public class Cliente implements Serializable {
 	public int getId() {
 		return this.id;
 	}
+	public void corregir_Datos(String nueva_tipoId, int nueva_id) {
+		this.tipoId = nueva_tipoId
+		this.id = nueva_id
+	}
+	
 	}
 
 
